1df142b5d0786608635ed264e4dffc8ebae05999
Murtaza <alimurtazabhatti0307@gmail.com>
2025-10-04 08:42:19 +0500

    sigin up

---
+++ enviromentobject.js
@@ -0,0 +1,7 @@
+export const enviromentobj = {
+  Project_id: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  Endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  Api_key: import.meta.env.APPWRITE_API_KEY,
+  Databaseid: import.meta.env.APPWRITE_DATABASE_ID,
+  Authcolletion: import.meta.env.APPWRITE_USERS_COLLECTION,
+};
--- main.js
+++ main.js
@@ -1,3 +1,45 @@
-export default async ({ req, res }) => {
-  return res.send("Hello from Appwrite Function!");
+import { Client, Databases, ID, Query } from "node-appwrite";
+import bcrypt from "bcryptjs";
+import { enviromentobj } from "./enviromentobject";
+
+export default async ({ req, res, log, error }) => {
+  try {
+    const client = new Client()
+      .setEndpoint(enviromentobj.Endpoint)
+      .setProject(enviromentobj.Project_id)
+      .setKey(enviromentobj.Api_key);
+
+    const databases = new Databases(client);
+
+    // Check if admin already exists
+    const result = await databases.listDocuments(
+      enviromentobj.Databaseid,
+      enviromentobj.Authcolletion,
+      [Query.limit(limit), Query.offset(offset), Query.orderDesc("$createdAt")]
+    );
+
+    if (result.total === 0) {
+      const rawPassword = "mypassword";
+      const hashedPassword = await bcrypt.hash(rawPassword, 10);
+
+      const adminDoc = await databases.createDocument(
+        enviromentobj.Databaseid,
+        enviromentobj.Authcolletion,
+        ID.unique(),
+        {
+          username: "admin@crm.com",
+          passwordHash: hashedPassword,
+          role: "admin",
+        }
+      );
+
+      log("✅ Admin created successfully");
+      return res.json({ success: true, adminDoc });
+    } else {
+      return res.json({ error: "⚠️ Admin already exists" });
+    }
+  } catch (err) {
+    error(err.message);
+    return res.json({ error: err.message });
+  }
 };
