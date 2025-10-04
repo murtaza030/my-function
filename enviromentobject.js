fa618f7531786dccaf9287baea40cc07a7d0e208
Murtaza <alimurtazabhatti0307@gmail.com>
2025-10-04 09:31:12 +0500

    dot

--- enviromentobject.js
+++ enviromentobject.js
@@ -1,7 +1,11 @@
-export const Enviromentobject = {
+import dotenv from "dotenv";
+dotenv.config();
+const Enviromentobject = {
   Project_id: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   Endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
   Api_key: import.meta.env.APPWRITE_API_KEY,
   Databaseid: import.meta.env.APPWRITE_DATABASE_ID,
   Authcolletion: import.meta.env.APPWRITE_USERS_COLLECTION,
 };
+
+export default Enviromentobject;
