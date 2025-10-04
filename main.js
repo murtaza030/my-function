cb0da5d215da87bb89044907bfc8c0a4695c1f72
Murtaza <alimurtazabhatti0307@gmail.com>
2025-10-04 20:55:53 +0500

    get

--- main.js
+++ main.js
@@ -1,45 +1,18 @@
-import { Client, Databases, ID, Query } from "node-appwrite";
-import bcrypt from "bcryptjs";
+import { Client, Databases } from "node-appwrite";
 import Enviromentobject from "./Enviromentobject.js";
 
 export default async ({ req, res, log, error }) => {
-  try {
-    const client = new Client()
-      .setEndpoint(Enviromentobject.Endpoint)
-      .setProject(Enviromentobject.Project_id)
-      .setKey(Enviromentobject.Api_key);
+  const client = new Client()
+    .setEndpoint(Enviromentobject.Endpoint)
+    .setProject(Enviromentobject.Project_id);
 
-    const databases = new Databases(client);
+  const db = new Databases(client);
 
-    // Check if admin already exists
-    const result = await databases.listDocuments(
+  if (req.method === "GET") {
+    const response = await db.listDocuments(
       Enviromentobject.Databaseid,
-      Enviromentobject.Authcolletion,
-      [Query.limit(limit), Query.offset(offset), Query.orderDesc("$createdAt")]
+      Enviromentobject.Authcolletion
     );
-
-    if (result.total === 0) {
-      const rawPassword = "mypassword";
-      const hashedPassword = await bcrypt.hash(rawPassword, 10);
-
-      const adminDoc = await databases.createDocument(
-        Enviromentobject.Databaseid,
-        Enviromentobject.Authcolletion,
-        ID.unique(),
-        {
-          username: "admin@crm.com",
-          passwordHash: hashedPassword,
-          role: "admin",
-        }
-      );
-
-      log("✅ Admin created successfully");
-      return res.json({ success: true, adminDoc });
-    } else {
-      return res.json({ error: "⚠️ Admin already exists" });
-    }
-  } catch (err) {
-    error(err.message);
-    return res.json({ error: err.message });
+    return res.json(response.documents);
   }
 };
