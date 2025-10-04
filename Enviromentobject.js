fdd2e09f27fbd47474f2e8251b3473814a7f227b
murtaza030 <50249571+murtaza030@users.noreply.github.com>
2025-10-04 09:17:41 +0500

    Update Enviromentobject.js

--- Enviromentobject.js
+++ Enviromentobject.js
@@ -1,7 +1,8 @@
-export const Enviromentobject = {
+const Enviromentobject = {
   Project_id: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   Endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
   Api_key: import.meta.env.APPWRITE_API_KEY,
   Databaseid: import.meta.env.APPWRITE_DATABASE_ID,
   Authcolletion: import.meta.env.APPWRITE_USERS_COLLECTION,
 };
+export default Enviromentobject;
