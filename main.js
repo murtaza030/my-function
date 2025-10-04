6bf46f059745654d0f5a662b1338e0477321e199
murtaza030 <50249571+murtaza030@users.noreply.github.com>
2025-10-04 21:14:06 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 import { Client, Databases } from "node-appwrite";
 import Enviromentobject from "./Enviromentobject.js";
-
+const db_id = process.env.APPWRITE_DATABASE_ID,
+const collection_id = process.env.APPWRITE_USERS_COLLECTION
 export default async ({ req, res, log, error }) => {
-  try {
+
     const client = new Client()
-      .setEndpoint(Enviromentobject.Endpoint)
-      .setProject(Enviromentobject.Project_id);
+      .setEndpoint(process.env.APPWRITE_ENDPOINT)
+      .setProject(process.env.APPWRITE_PROJECT_ID);
 
     const db = new Databases(client);
 
     if (req.method === "GET") {
       const response = await db.listDocuments(
-        Enviromentobject.Databaseid,
-        Enviromentobject.Authcolletion
+        db_id,
+        collection_id
       );
       return res.json(response.documents);
     }
-  } catch (err) {
-    error(err.message);
-    return res.json({ success: false, error: err.message });
-  }
 };
 
