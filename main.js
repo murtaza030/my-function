c1e8364548a220e03950148bed72837890c05f80
murtaza030 <50249571+murtaza030@users.noreply.github.com>
2025-10-05 12:27:25 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,24 +2,41 @@ import { Client, Databases } from "node-appwrite";
 
 const db_id = process.env.APPWRITE_DATABASE_ID;
 const collection_id = process.env.APPWRITE_USERS_COLLECTION;
-export default async ({ req, res, log, error }) => {
 
+export default async ({ req, res, log, error }) => {
+  try {
     const client = new Client()
       .setEndpoint(process.env.VITE_APPWRITE_ENDPOINT)
       .setProject(process.env.VITE_APPWRITE_PROJECT_ID);
 
     const db = new Databases(client);
-    if (req.method === "GET") {
-        const id = "55555"
-        const response = await db.createDocument(
-      db_id, 
-      collection_id,    
-      id,                            
-      {
-        firstName:"murtaza"
-      }
-    );
-      return res.json(response.documents);
-    }
-};
 
+    if (req.method === "GET") {
+      const id = "55555";
+      const response = await db.createDocument(
+        db_id,
+        collection_id,
+        id,
+        {
+          firstName: "murtaza",
+        }
+      );
+
+      return res.json({
+        success: true,
+        data: response,
+      });
+    }
+
+    return res.json({
+      success: false,
+      error: "Unsupported request method",
+    });
+  } catch (err) {
+    log("Error creating document:", err.message);
+    return res.json({
+      success: false,
+      error: err.message,
+    });
+  }
+};
