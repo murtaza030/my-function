debbdd413ed8b9e5e6f12b5c49549143054472d8
murtaza030 <50249571+murtaza030@users.noreply.github.com>
2025-10-10 19:54:28 +0500

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ export default async ({ req, res, log, error }) => {
 
     const client = new Client()
       .setEndpoint(process.env.VITE_APPWRITE_ENDPOINT)
-      .setProject(process.env.VITE_APPWRITE_PROJECT_ID);
+      .setProject(process.env.VITE_APPWRITE_PROJECT_ID)
       .setKey(process.env.APPWRITE_API_KEY);
 
     const db = new Databases(client);
